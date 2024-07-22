1d586c36175ca69d7bc21972c2ceb40b149dca82
Amit Cesar <amitcesar27@gmail.com>
2024-07-22 20:59:13 -0300

    second commit

--- composeApp/src/commonMain/kotlin/ui/Home.kt
+++ composeApp/src/commonMain/kotlin/ui/Home.kt
@@ -1,15 +1,1 @@
 
-@Composable
-fun Home(event: Event){
-    Card(
-        modifier = Modifier.size(300.dp).padding(vertical = 10.dp).clickable {
-
-        },
-        backgroundColor = Color.Cyan
-    ) {
-        Column {
-            Text(Home)
-
-        }
-    }
-}
